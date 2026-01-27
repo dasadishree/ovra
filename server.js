3b79de70fcab77fdefb26821517184a32c391f5d
Adishree Das <dasadishree@gmail.com>
2026-01-26 20:16:34 -0500

    host fix

--- server.js
+++ server.js
@@ -22,4 +22,5 @@ app.post('/chat', async (req, res) => {
     }
 });
 
-app.listen(3000, ()=> console.log(`Server running`));
+const PORT = process.env.PORT || 3000;
+app.listen(3000, ()=> console.log(`Server running on port ${PORT}`));
